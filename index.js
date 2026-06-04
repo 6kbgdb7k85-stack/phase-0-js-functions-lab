47978d84c53c54866ed5e62946beede5bc948d17
6kbgdb7k85-stack <6kbgdb7k86@privaterelay.appleid.com>
2026-06-04 15:15:39 -0400

    Lab Complete

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
+function calculateTax(amount){
+    const taxRate = 0.10
+    return amount*taxRate
+}
 
+function convertToUpperCase(string){
+    let upperCaseString = '';
+    for (let i=0;i<string.length;i++){
+        console.log(string[i]);
+        upperCaseString+=(string[i].toUpperCase())
+    }
+    return upperCaseString;
+}
+
+function findMaximum(num1,num2){
+    let largerNumber;
+    if(num1>num2){
+        largerNumber=num1;
+    }else if (num2>num1){
+        largerNumber=num2;
+    }else {
+        return num1
+    }
+    return largerNumber
+}
+
+function isPalindrome(word){
+    let reverseWord = '';
+    for (let i=word.length-1;i>=0;i--){
+        reverseWord+=word[i];
+    }
+    if(reverseWord===word){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage){
+    return originalPrice-(originalPrice*(discountPercentage/100));
+}
 
 
 
